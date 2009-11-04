6c29c1528a4dd5c9777e2ddfa7b6f8b9508c98b6
Joe Blaylock <jrbl@slac.stanford.edu>
2009-11-04 11:53:01 -0800

    suggest: working back-end for affils
    Gathers up the previous affiliations for an author and streams them out.

--- modules/bibedit/lib/suggest.py
+++ modules/bibedit/lib/suggest.py
@@ -4,48 +4,52 @@ from invenio.search_engine import search_pattern, print_record
 from invenio.bibrecord import create_record, record_get_field_values, record_get_field_value
 
 
-def search_forward(l, s):
-    """Find the first item in l which contains a lowercase match for s"""
-    for i in range(len(l)):
-        if s.lower() in l[i].lower():
-            return i
-    return -1
+def record_auPairs(recID):
+    yieldedSomething = False
+    record = create_record(print_record(recID, 'xm'))[0]
+    
+    def extract(l):
+        a = None
+        u = None
+        for key, val in l:
+            if key == 'a':
+                a = val
+            elif key == 'u':
+                u = val
+        return a, u
 
-def search_backward(l, ci, ti):
-    """Search backward through list l, finding first tuple whose index != None"""
-    for i in range(ci, -1, -1):
-        if l[i] == None:
-            continue
-        if isinstance(l[i], type(tuple())):
-            if l[i][ti] != None:
-                return l[i][ti]
-        continue
-    return None
+    try:
+        yield extract( record['100'][0][0] )
+        yieldedSomething = True
+    except KeyError:
+        pass
 
-def suggestAffils(name):
+    try:
+        fields = record['700']
+    except KeyError:
+        if not yieldedSomething:
+            yield None, None
+        return
+
+    for fieldline in fields:
+        tuplist = fieldline[0]
+        yield extract( tuplist )
+
+def suggestions(name):
     """Given an author name pattern, find all possible prior affiliations""" 
 
+    previousGood = None
+
     search_results = sorted( search_pattern( p=search_for, ap=1 ), reverse=True )
-    item_count = len(search_results)
 
-    record_list = [create_record(print_record(x, 'xm'))[0] for x in search_results]
-
-    pauth_affils = [None] * item_count
-    for i in range(item_count):
-
-        authors = record_get_field_value
-        primary_authors = record_get_field_values(record_list[i], '100', '%', '%', '%')
-
-        x = 2 * search_forward(primary_authors[::2], search_for)
-        if x < 0:
-            continue 
-
-        try:
-            pauth_affils[i] = (primary_authors[x+1], primary_authors[x])
-        except IndexError, msg:
-            pauth_affils[i] = (search_backward(pauth_affils, i, 0), primary_authors[x])
-
-    return pauth_affils
+    for recID in search_results:
+        for author, affiliation in record_auPairs(recID):
+            if name.lower() in author.lower():
+                if affiliation != None:
+                    previousGood = affiliation
+                    yield recID, author, affiliation
+                else:
+                    yield recID, author, previousGood
 
 
 if __name__ == "__main__":
@@ -57,10 +61,8 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         search_for = sys.argv[1]
 
-    pauth_affils = suggestAffils(search_for)
-
     print search_for
-    for i in range(len(pauth_affils)):
-        if pauth_affils[i] != None:
-            print '\t', pauth_affils[i]
+    for recID, auth, affil in suggestions(search_for):
+        if affil != None:
+            print '\t%5d %35s %35s' % (recID, '"'+auth+'"', '"'+affil+'"')
 
